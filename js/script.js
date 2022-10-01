2133b1e87cd94218003baa3f9103a69d8c5c8268
Claudio128 <cllaudio128@gmail.com>
2022-10-01 20:24:41 -0300

    Upload dos arquivos

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function facebook() {
+    document.getElementById("facebook").src="https://www.facebook.com/claudioandrade123"
+}
+
+function github() {
+    document.getElementById("github").href="https://github.com/Claudio128"
+}
+
+function instagram() {
+    document.getElementById("instagram").href="https://www.instagram.com/claudio_aandrade/"
+}
+
+function linkedin() {
+    document.getElementById("linkedin").href="https://www.linkedin.com/in/claudio-andrade-107bbb204/"
+}
+
+function telegram() {
+    document.getElementById("telegram").href="https://t.me/MAG0128"
+}
